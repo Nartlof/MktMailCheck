a15df796772ea7fc30057d430159872bf3537797
Nartlof <eduardo@foltran.sampa.br>
2018-08-29 14:46:37 -0300

    Validação e Importação de endereços

--- MktMailCheck/MktMailCheck.Designer.vb
+++ MktMailCheck/MktMailCheck.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MktMailCheck
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,170 @@ Partial Class MktMailCheck
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.MktMailCheckMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.ArquivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EmailsToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SpamTrapsToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImportarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EmailsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SpamTrapsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExportarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AjudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenCsvFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveCsvFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.BarraDeStatus = New System.Windows.Forms.StatusStrip()
+        Me.StatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ProgressoLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MktMailCheckMenuStrip.SuspendLayout()
+        Me.BarraDeStatus.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MktMailCheckMenuStrip
+        '
+        Me.MktMailCheckMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArquivoToolStripMenuItem, Me.EditarToolStripMenuItem, Me.ImportarToolStripMenuItem, Me.ExportarToolStripMenuItem, Me.AjudaToolStripMenuItem})
+        Me.MktMailCheckMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MktMailCheckMenuStrip.Name = "MktMailCheckMenuStrip"
+        Me.MktMailCheckMenuStrip.Size = New System.Drawing.Size(637, 24)
+        Me.MktMailCheckMenuStrip.TabIndex = 0
+        Me.MktMailCheckMenuStrip.Text = "MailCheckMenuStrip"
+        '
+        'ArquivoToolStripMenuItem
+        '
+        Me.ArquivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SairToolStripMenuItem})
+        Me.ArquivoToolStripMenuItem.Name = "ArquivoToolStripMenuItem"
+        Me.ArquivoToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.ArquivoToolStripMenuItem.Text = "&Arquivo"
+        '
+        'SairToolStripMenuItem
+        '
+        Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
+        Me.SairToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
+        Me.SairToolStripMenuItem.Text = "&Sair"
+        '
+        'EditarToolStripMenuItem
+        '
+        Me.EditarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EmailsToolStripMenuItem1, Me.SpamTrapsToolStripMenuItem1})
+        Me.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
+        Me.EditarToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.EditarToolStripMenuItem.Text = "&Editar"
+        '
+        'EmailsToolStripMenuItem1
+        '
+        Me.EmailsToolStripMenuItem1.Name = "EmailsToolStripMenuItem1"
+        Me.EmailsToolStripMenuItem1.Size = New System.Drawing.Size(132, 22)
+        Me.EmailsToolStripMenuItem1.Text = "&Emails"
+        Me.EmailsToolStripMenuItem1.ToolTipText = "Editar a lista de endereços de e-mail"
+        '
+        'SpamTrapsToolStripMenuItem1
+        '
+        Me.SpamTrapsToolStripMenuItem1.Name = "SpamTrapsToolStripMenuItem1"
+        Me.SpamTrapsToolStripMenuItem1.Size = New System.Drawing.Size(132, 22)
+        Me.SpamTrapsToolStripMenuItem1.Text = "&SpamTraps"
+        Me.SpamTrapsToolStripMenuItem1.ToolTipText = "Editar a lista de servidores spamtrap"
+        '
+        'ImportarToolStripMenuItem
+        '
+        Me.ImportarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EmailsToolStripMenuItem, Me.SpamTrapsToolStripMenuItem})
+        Me.ImportarToolStripMenuItem.Name = "ImportarToolStripMenuItem"
+        Me.ImportarToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.ImportarToolStripMenuItem.Text = "&Importar"
+        '
+        'EmailsToolStripMenuItem
+        '
+        Me.EmailsToolStripMenuItem.Name = "EmailsToolStripMenuItem"
+        Me.EmailsToolStripMenuItem.Size = New System.Drawing.Size(132, 22)
+        Me.EmailsToolStripMenuItem.Text = "&Emails"
+        Me.EmailsToolStripMenuItem.ToolTipText = "Importar lista com endereços de e-mail"
+        '
+        'SpamTrapsToolStripMenuItem
+        '
+        Me.SpamTrapsToolStripMenuItem.Name = "SpamTrapsToolStripMenuItem"
+        Me.SpamTrapsToolStripMenuItem.Size = New System.Drawing.Size(132, 22)
+        Me.SpamTrapsToolStripMenuItem.Text = "&SpamTraps"
+        Me.SpamTrapsToolStripMenuItem.ToolTipText = "Importar lista com servidores armadilha"
+        '
+        'ExportarToolStripMenuItem
+        '
+        Me.ExportarToolStripMenuItem.Name = "ExportarToolStripMenuItem"
+        Me.ExportarToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
+        Me.ExportarToolStripMenuItem.Text = "E&xportar"
+        '
+        'AjudaToolStripMenuItem
+        '
+        Me.AjudaToolStripMenuItem.Name = "AjudaToolStripMenuItem"
+        Me.AjudaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.AjudaToolStripMenuItem.Text = "Aj&uda"
+        '
+        'OpenCsvFileDialog
+        '
+        Me.OpenCsvFileDialog.Filter = "Arquivo CSV *.csv|*.csv|Arquvos de texto *.txt|*.txt|Todos os arquivos *.*|*.*"
+        Me.OpenCsvFileDialog.RestoreDirectory = True
+        '
+        'SaveCsvFileDialog
+        '
+        Me.SaveCsvFileDialog.CheckFileExists = True
+        Me.SaveCsvFileDialog.Filter = "Arquivo CSV *.csv|*.csv|Arquvos de texto *.txt|*.txt|Todos os arquivos *.*|*.*"
+        Me.SaveCsvFileDialog.RestoreDirectory = True
+        '
+        'BarraDeStatus
+        '
+        Me.BarraDeStatus.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusLabel, Me.ProgressoLabel})
+        Me.BarraDeStatus.Location = New System.Drawing.Point(0, 239)
+        Me.BarraDeStatus.Name = "BarraDeStatus"
+        Me.BarraDeStatus.Size = New System.Drawing.Size(637, 22)
+        Me.BarraDeStatus.TabIndex = 1
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(43, 17)
+        Me.StatusLabel.Text = "Pronto"
+        '
+        'ProgressoLabel
+        '
+        Me.ProgressoLabel.Name = "ProgressoLabel"
+        Me.ProgressoLabel.Size = New System.Drawing.Size(10, 17)
+        Me.ProgressoLabel.Text = ":"
         '
         'MktMailCheck
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(637, 261)
+        Me.Controls.Add(Me.BarraDeStatus)
+        Me.Controls.Add(Me.MktMailCheckMenuStrip)
+        Me.MainMenuStrip = Me.MktMailCheckMenuStrip
         Me.Name = "MktMailCheck"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Mkt Mail Check"
+        Me.MktMailCheckMenuStrip.ResumeLayout(False)
+        Me.MktMailCheckMenuStrip.PerformLayout()
+        Me.BarraDeStatus.ResumeLayout(False)
+        Me.BarraDeStatus.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MktMailCheckMenuStrip As MenuStrip
+    Friend WithEvents ArquivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImportarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AjudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EmailsToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SpamTrapsToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents EmailsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SpamTrapsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenCsvFileDialog As OpenFileDialog
+    Friend WithEvents SaveCsvFileDialog As SaveFileDialog
+    Friend WithEvents BarraDeStatus As StatusStrip
+    Friend WithEvents StatusLabel As ToolStripStatusLabel
+    Friend WithEvents ProgressoLabel As ToolStripStatusLabel
 End Class
--- MktMailCheck/MktMailCheck.vb
+++ MktMailCheck/MktMailCheck.vb
@@ -1,60 +1,182 @@
 ﻿Imports System.IO
 Imports System.Data.OleDb
+Imports System.ComponentModel
+Imports System.Text.RegularExpressions
 
 Public Class MktMailCheck
 
     Shared ReadOnly Property MktDbFile As String = Application.StartupPath & "\MktDbFile.mdb"
     Shared ReadOnly Property ConnectionString As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & MktDbFile
+
+#Region "Eventos do formulario"
     Private Sub MktMailCheck_Load(sender As Object, e As EventArgs) Handles Me.Load
         'Se o banco de dados não existe, deve ser criado
         If Not File.Exists(MktDbFile) Then
-            Dim Catalog As New ADOX.Catalog
-            Catalog.Create(ConnectionString)
-            Using Conn As New OleDbConnection(ConnectionString)
-                Using Comm As New OleDbCommand("", Conn)
-                    Conn.Open()
-                    'Criando a tabela que vai conter os endereços de email
-                    'Campo email contem o endereço propriamente dito. É definido como primário para não haver repetição
-                    'Campo VrStatus diz o estatus da verificação:
-                    '    0 - Valor padrão - não verificado
-                    '    1 - Sinal verde - verificado e válido. Pode enviar.
-                    '    2 - Sinal azul - não há como saber sem enviar uma mensagem.
-                    '    3 - Sinal laranja - verificado como "pega-tudo". Não há garantia de caixa postal única
-                    '    4 - Sinal vermelho - endereço inválido. Não vale a pena mandar pois será bounce
-                    '    5 - Sinal preto - provável armadilha
-                    'Campo VrDate contem a data da última verificação do estatus para verificação a intervalos de tempo.
-                    Comm.CommandText = "CREATE TABLE EmailTable (
+            MsgBox("Não foi encontrado o banco de dados. Um novo será criado.", MsgBoxStyle.Exclamation, "Erro ao abrir o banco")
+            CreateNewDatabase()
+        End If
+        SetFormPosition()
+    End Sub
+
+    Private Sub MktMailCheck_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If MsgBox("Quer mesmo sair e encerrar o programa?", MsgBoxStyle.YesNo, "Confirmação") = MsgBoxResult.No Then
+            e.Cancel = True
+        Else
+            SaveFormPosition()
+        End If
+    End Sub
+
+#End Region
+
+#Region "Itens do Menu"
+
+    Private Sub SairToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SairToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub EmailsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EmailsToolStripMenuItem.Click
+        Dim EmailStream As Stream = Nothing
+        If OpenCsvFileDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                EmailStream = OpenCsvFileDialog.OpenFile()
+                If (EmailStream IsNot Nothing) Then
+                    Dim MailRegex As New Regex("^[a-z]\w+([-+.]\w+)*@[a-z]\w+([-.]\w+)*\.\w+([-.]\w+)*[a-z]$")
+                    Dim StLable As ToolStripItem = BarraDeStatus.Items.Item(1)
+                    Dim Endereco As String = ""
+                    Dim Encontrados As Integer = 0
+                    Dim Adicionados As Integer = 0
+                    BarraDeStatus.Items.Item(0).Text = "Importando:"
+                    StLable.Text = ""
+                    Using Conn As New OleDbConnection(ConnectionString)
+                        Using Comm As New OleDbCommand("", Conn)
+                            Dim MailAdress As New OleDbParameter("@Email", "")
+                            Comm.Parameters.Add(MailAdress)
+                            Using Reader As New StreamReader(EmailStream)
+                                Conn.Open()
+                                While Not Reader.EndOfStream
+                                    Endereco = Reader.ReadLine
+                                    Endereco = Endereco.ToLower.Trim
+                                    If MailRegex.IsMatch(Endereco) Then
+                                        Comm.CommandText = "Select Count(*) from EmailTable where Email=@Email"
+                                        MailAdress.Value = Endereco
+                                        Encontrados += 1
+                                        If Comm.ExecuteScalar = 0 Then
+                                            Comm.CommandText = "Insert Into EmailTable (Email) Values (@Email)"
+                                            Comm.ExecuteNonQuery()
+                                            Adicionados += 1
+                                        End If
+                                    End If
+                                    StLable.Text = String.Format("{0} endereços adcionados de {1} encontrados", Adicionados, Encontrados)
+                                End While
+                                Conn.Close()
+                            End Using
+                        End Using
+                    End Using
+                End If
+            Catch Ex As Exception
+                MessageBox.Show("Erro ao ler o arquivo: " & Ex.Message)
+            Finally
+                ' Check this again, since we need to make sure we didn't throw an exception on open.
+                If (EmailStream IsNot Nothing) Then
+                    EmailStream.Close()
+                End If
+            End Try
+        End If
+    End Sub
+#End Region
+
+#Region "Rotinas de suporte"
+    Private Sub SetFormPosition()
+        If (ModifierKeys And Keys.Shift) = 0 Then
+            Dim initLocation As String = My.Settings.InitialLocation
+            Dim il As Point = New Point(0, 0)
+            Dim sz As Size = Size
+            Dim St As FormWindowState = FormWindowState.Normal
+            If Not String.IsNullOrWhiteSpace(initLocation) Then
+                Dim parts As String() = initLocation.Split(","c)
+                If parts.Length >= 2 Then
+                    il = New Point(Integer.Parse(parts(0)), Integer.Parse(parts(1)))
+                End If
+                If parts.Length >= 4 Then
+                    sz = New Size(Integer.Parse(parts(2)), Integer.Parse(parts(3)))
+                End If
+                If parts.Length >= 5 Then
+                    St = [Enum].Parse(St.GetType, parts(4))
+                End If
+            End If
+            Size = sz
+            Location = il
+            WindowState = St
+        End If
+    End Sub
+    Private Sub SaveFormPosition()
+        If (ModifierKeys And Keys.Shift) = 0 Then
+            Dim loc As Point = Location
+            Dim siz As Size = Size
+            Dim WSt As FormWindowState = WindowState
+            If WSt = FormWindowState.Minimized Then WSt = FormWindowState.Normal
+            If WindowState <> FormWindowState.Normal Then
+                loc = RestoreBounds.Location
+                siz = RestoreBounds.Size
+            End If
+            Dim initLocation As String = String.Join(",", loc.X, loc.Y, siz.Width, siz.Height, WSt)
+            My.Settings.InitialLocation = initLocation
+            My.Settings.Save()
+        End If
+    End Sub
+    Private Sub CreateNewDatabase()
+        Dim Catalog As New ADOX.Catalog
+        Catalog.Create(ConnectionString)
+
+        Using Conn As New OleDbConnection(ConnectionString)
+            Using Comm As New OleDbCommand("", Conn)
+                Conn.Open()
+                'Criando a tabela que vai conter os endereços de email
+                'Campo email contem o endereço propriamente dito. É definido como primário para não haver repetição
+                'Campo VrStatus diz o estatus da verificação:
+                '    0 - Valor padrão - não verificado
+                '    1 - Sinal verde - verificado e válido. Pode enviar.
+                '    2 - Sinal azul - não há como saber sem enviar uma mensagem.
+                '    3 - Sinal laranja - verificado como "pega-tudo". Não há garantia de caixa postal única
+                '    4 - Sinal vermelho - endereço inválido. Não vale a pena mandar pois será bounce
+                '    5 - Sinal preto - provável armadilha
+                'Campo VrDate contem a data da última verificação do estatus para verificação a intervalos de tempo.
+                Comm.CommandText = "CREATE TABLE EmailTable (
                                               Email varchar(250) NOT NULL PRIMARY KEY,
                                               VrStatus int NOT NULL DEFAULT 0,
                                               VrDate date NOT NULL DEFAULT Now())"
-                    Comm.ExecuteNonQuery()
-                    'Criando a tabela de servidores smtp. Serve para marcar quais são armadilha ou pega-tudo e define o intervalo de tempo entre chamadas
-                    'Campo ServerDomain contem o domínio propriamente dito
-                    'Campo MxServer contem o servidor MX principal
-                    'Campo MxIP contem o IP do MX
-                    'Campo VrStatus contem o status do servidor
-                    '    0 - não verificado
-                    '    1 - operacional - normal
-                    '    2 - operacional - Não informa status sem envio. Não adianta verificar
-                    '    3 - operacional - pega-tudo Não precisa testar e-mails pois vai aceitar tudo.
-                    '    4 - falhado - Não enviar mensagem até a próxima verificação
-                    '    5 - armadilha - é definido caso o servidor MX esteja listado na tabela de armadilhas spamtrap
-                    'Campo VrDate contem a data da última verificação do servidor
-                    'Campo LastSend contem a data e hora da última verificação de endereço. Só usa nos operacionais normais
-                    Comm.CommandText = "CREATE TABLE SmtpServers (
+                Comm.ExecuteNonQuery()
+                'Criando a tabela de servidores smtp. Serve para marcar quais são armadilha ou pega-tudo e define o intervalo de tempo entre chamadas
+                'Campo ServerDomain contem o domínio propriamente dito
+                'Campo MxServer contem o servidor MX principal
+                'Campo MxIP contem o IP do MX
+                'Campo VrStatus contem o status do servidor
+                '    0 - não verificado
+                '    1 - operacional - normal
+                '    2 - operacional - Não informa status sem envio. Não adianta verificar
+                '    3 - operacional - pega-tudo Não precisa testar e-mails pois vai aceitar tudo.
+                '    4 - falhado - Não enviar mensagem até a próxima verificação
+                '    5 - armadilha - é definido caso o servidor MX esteja listado na tabela de armadilhas spamtrap
+                'Campo VrDate contem a data da última verificação do servidor
+                'Campo LastSend contem a data e hora da última verificação de endereço. Só usa nos operacionais normais
+                Comm.CommandText = "CREATE TABLE SmtpServers (
                                             ServerDomain varchar(250) NOT NULL PRIMARY KEY,
                                             MxServer varchar(250) NOT NULL DEFAULT '',
                                             MxIP varchar(15) NOT NULL DEFAULT '',
                                             VrStatus int  NOT NULL DEFAULT 0,
                                             VrDate date NOT NULL DEFAULT Now(),
                                             LastSend datetime NOT NULL DEFAULT Now())"
-                    Comm.ExecuteNonQuery()
-                    'Criando tabela de armadilhas. Contem os nomes dos servidores MX que correspondem a armadilhas de spamtrap
-                    'Campo ServerName contem o nome do servidor spamtrap
-                    Comm.CommandText = "CREATE TABLE SpamTraps (SpamTrapName varchar(250) NOT NULL PRIMARY KEY)"
-                    Conn.Close()
-                End Using
+                Comm.ExecuteNonQuery()
+                'Criando tabela de armadilhas. Contem os nomes dos servidores MX que correspondem a armadilhas de spamtrap
+                'Campo ServerName contem o nome do servidor spamtrap
+                Comm.CommandText = "CREATE TABLE SpamTraps (ServerName varchar(250) NOT NULL PRIMARY KEY)"
+                Conn.Close()
             End Using
-        End If
+        End Using
     End Sub
+
+
+
+#End Region
+
 End Class
